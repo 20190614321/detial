2496fc35035f63b6602d24162e719127110684b3
LuckyMan <3208128691@qq.com>
2020-07-31 12:28:03 +0800

    2.自己创建一个容器

--- app/src/main/java/com/example/detial/MainActivity.kt
+++ app/src/main/java/com/example/detial/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.detial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+    }
+    fun addLinearLayout(){
+        //1.创建工程
+        val linearLayout = LinearLayout(this)
+        //2.给容器设置宽 高
+        linearLayout.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT)
+        //3.设置布局方向
+        linearLayout.orientation = LinearLayout.VERTICAL
+        //4.修改背景颜色
+        linearLayout.background = getDrawable(R.color.colorAccent)
+        //5.添加容器到Activity
+        setContentView(linearLayout)
     }
 }
