632732ac75b621cc71a4685662ca72a5a6b20599
LuckyMan <3208128691@qq.com>
2020-07-31 17:20:58 +0800

    4.创建第一个子控件以及屏幕密度与像素的转换

--- app/src/main/java/com/example/detial/MainActivity.kt
+++ app/src/main/java/com/example/detial/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : AppCompatActivity() {
     }
     private fun addLinearLayoutKotlin(){
         //创建容器
-        LinearLayout(this).apply {
+        val container = LinearLayout(this).apply {
             //设置宽 高
             layoutParams = LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
@@ -40,5 +40,21 @@ class MainActivity : AppCompatActivity() {
             background = getDrawable(R.color.colorAccent)
             setContentView(this)
         }
+        //创建第一个子控件
+        LinearLayout(this).apply {
+            //设置宽 高
+            layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                dp2x(100)
+            )
+            //方向
+            orientation = LinearLayout.HORIZONTAL
+            //背景颜色
+            background = getDrawable(R.color.colorPrimary)
+            container.addView(this)
+        }
+    }
+    fun dp2x(dp:Int):Int{
+        return (resources.displayMetrics.density * dp).toInt()
     }
 }
