cbaf80f6cc60ea591b1fba7d9bb77b83618ad9b1
LuckyMan <3208128691@qq.com>
2020-07-31 12:43:29 +0800

    3.修改代码为Kotlin风格

--- app/src/main/java/com/example/detial/MainActivity.kt
+++ app/src/main/java/com/example/detial/MainActivity.kt
@@ -9,8 +9,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
+        //addLinearLayout()
+        addLinearLayoutKotlin()
     }
-    fun addLinearLayout(){
+    fun addLinearLayoutjava(){
         //1.创建工程
         val linearLayout = LinearLayout(this)
         //2.给容器设置宽 高
@@ -23,5 +25,20 @@ class MainActivity : AppCompatActivity() {
         linearLayout.background = getDrawable(R.color.colorAccent)
         //5.添加容器到Activity
         setContentView(linearLayout)
+    }
+    private fun addLinearLayoutKotlin(){
+        //创建容器
+        LinearLayout(this).apply {
+            //设置宽 高
+            layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+            )
+            //方向
+            orientation = LinearLayout.VERTICAL
+            //背景颜色
+            background = getDrawable(R.color.colorAccent)
+            setContentView(this)
+        }
     }
 }
